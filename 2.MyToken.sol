34b2715d184cd6db21c73abc8133991ee8637d4d
Nattapon33998 <nattapon33998@gmail.com>
2024-08-22 13:16:50 +0700

    add event

--- 2.MyToken.sol
+++ 2.MyToken.sol
@@ -2,19 +2,19 @@
 pragma solidity ^0.8.0;
 
 contract MyToken {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(
         address indexed owner,
         address indexed spender,
         uint256 value
     );
-
-    uint256 public totalSupply;
-    mapping(address => uint256) public balanceOf;
-    mapping(address => mapping(address => uint256)) public allowance;
-    string public name;
-    string public symbol;
-    uint8 public decimals;
 
     constructor(string memory _name, string memory _symbol, uint8 _decimals) {
         name = _name;
